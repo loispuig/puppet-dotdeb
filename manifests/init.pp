20aa584395a1f317d2802904ceb51c67f7694d71
Loïs <lois.puig@kctus.fr>
2016-05-11 11:21:08 +0200

    Add $pin variable

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,43 +1,19 @@
 # == Class: dotdeb
 #
-# Full description of class dotdeb here.
-#
-# === Parameters
-#
-# Document parameters here.
-#
-# [*sample_parameter*]
-#   Explanation of what this parameter affects and what it defaults to.
-#   e.g. "Specify one or more upstream ntp servers as an array."
-#
-# === Variables
-#
-# Here you should define a list of variables that this module would require.
-#
-# [*sample_variable*]
-#   Explanation of how this variable affects the funtion of this class and if
-#   it has a default. e.g. "The parameter enc_ntp_servers must be set by the
-#   External Node Classifier as a comma separated list of hostnames." (Note,
-#   global variables should be avoided in favor of class parameters as
-#   of Puppet 2.6.)
-#
-# === Examples
-#
-#  class { 'dotdeb':
-#    servers => [ 'pool.ntp.org', 'ntp.local.company.com' ],
-#  }
+# Setup dotdeb repository on Debian OS
 #
 # === Authors
 #
-# Author Name <author@domain.com>
+# Author Loïs PUIG <lois.puig@kctus.fr>
 #
 # === Copyright
 #
-# Copyright 2016 Your name here, unless otherwise noted.
+# Copyright 2016 Loïs PUIG, unless otherwise noted.
 #
-class dotdeb {
-
-	anchor { 'dotdeb::begin': } ->
-		class { '::dotdeb::apt': } ->
-	anchor { 'dotdeb::end': }
+class dotdeb (
+	$pin = 750,
+) {
+	anchor { "dotdeb::begin": } ->
+		class { "::dotdeb::apt": } ->
+	anchor { "dotdeb::end": }
 }
