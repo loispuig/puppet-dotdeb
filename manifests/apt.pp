4732c6da074223b1476a657a43b55c24bb3882a8
Loïs <lois.puig@kctus.fr>
2016-05-11 11:20:22 +0200

    Conditional code execution based on OS detection

--- manifests/apt.pp
+++ manifests/apt.pp
@@ -1,21 +1,27 @@
 class dotdeb::apt inherits dotdeb {
+	if $::osfamily == 'debian' {
+		apt::source { "dotdeb":
+			location => "http://packages.dotdeb.org",
+			release  => $::lsbdistcodename,
+			pin      => $pin,
+			repos    => "all",
+			include  => {
+				"src" => true,
+				"deb" => true,
+			},
+			notify => Exec["dotdeb-apt-key"],
+		}
 
-	apt::source { "dotdeb":
-		location => "http://packages.dotdeb.org",
-		release => $lsbdistcodename,
-		repos => "all",
-		include => {
-			'src' => true,
-			'deb' => true,
-		},
-		notify => Exec['dotdeb-apt-key'],
+		exec { "dotdeb-apt-key":
+			cwd		=> "/tmp",
+			command => "curl -L --silent 'http://www.dotdeb.org/dotdeb.gpg' | apt-key add -",
+			unless  => "apt-key list | grep -q dotdeb",
+			path    => ["/bin/", "/sbin/" , "/usr/bin/", "/usr/sbin/"],
+			require => Apt::Source["dotdeb"],
+			notify  => Exec["apt_update"],
+		}
 	}
-
-	exec { 'dotdeb-apt-key':
-		command => 'curl -L --silent "http://www.dotdeb.org/dotdeb.gpg" | apt-key add -',
-		unless  => 'apt-key list | grep -q dotdeb',
-		path    => [ '/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/' ],
-		require => File['/etc/apt/sources.list.d/dotdeb.list'],
-		notify  => Exec['apt_update'],
+	else {
+		fail("Unsupported platform: ${::operatingsystem}")
 	}
 }
